c1b5af7af9af79b1b150dba3a781fdc99a28b65c
Nidhi Jamar <nidhi.jmr@gmail.com>
2019-02-16 14:31:57 -0800

    initial commit

---
+++ app/src/main/java/edu/sjsu/silversnugmobileapp/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.sjsu.silversnugmobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHelloWorld;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvHelloWorld = findViewById(R.id.tvHelloWorld);
+        tvHelloWorld.setText("Hello! Welcome");
+    }
+}
